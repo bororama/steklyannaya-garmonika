4745f63fcc1a7565c4a79dfc9451aa786cd168f0
Nicolas Pinto gomez <npinto-g@student.42madrid.com>
2024-01-16 18:49:04 +0100

    HOTFIX - shared folder required before mounting

---
+++ src/backend/db-api/src/meta/shared/meta.interface.ts
@@ -0,0 +1,32 @@
+
+export interface User {
+  locator: number;
+  name: string;
+} 
+
+export interface Message {
+  user: User;
+  text: string;
+}
+
+export interface Player {
+  user: User;
+  position: Array<number>;
+  rotation: Array<number>;
+  state : number
+}
+
+export interface LiveClient { 
+  socket : any; //Socket
+  player : Player | null;
+};
+
+export interface ServerToClientEvents {
+  chat: (message : Message) => void;
+  newPlayer: (player : Player) => void;
+}
+
+export interface ClientToServerEvents {
+  chat: (message : Message) => void;
+  playerUpdate : (player : Player) => void;
+}
