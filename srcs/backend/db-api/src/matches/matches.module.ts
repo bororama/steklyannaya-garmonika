c31d31b84ad2e64ba5fa856ff64d80611b01d973
Francisco Jesus Gata valdes <fgata-va@student.42madrid.com>
2024-02-13 23:52:44 +0100

    Matches controller protected by middleware

--- srcs/backend/db-api/src/matches/matches.module.ts
+++ srcs/backend/db-api/src/matches/matches.module.ts
@@ -1,15 +1,24 @@
-import { Module, ValidationPipe, forwardRef } from '@nestjs/common';
+import { Module, ValidationPipe, forwardRef, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Match } from './models/match.model';
 import { MatchesController } from './matches.controller';
 import { MatchesService } from './matches.service';
 import { APP_PIPE } from '@nestjs/core';
 import { UsersModule } from '../users/users.module';
+import { AuthMiddleware } from 'src/middleware/auth-middleware';
+import { AuthenticMiddleware } from 'src/middleware/authenticity-middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [SequelizeModule.forFeature([Match]), forwardRef(() => UsersModule)],
+    imports: [SequelizeModule.forFeature([Match]), forwardRef(() => UsersModule), ConfigModule],
     controllers: [MatchesController],
     providers: [MatchesService,  { provide: APP_PIPE, useClass: ValidationPipe }],
     exports: [MatchesService]
 })
-export class MatchesModule {}
+export class MatchesModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(AuthMiddleware, AuthenticMiddleware)
+                .forRoutes(MatchesController);
+    }
+}
